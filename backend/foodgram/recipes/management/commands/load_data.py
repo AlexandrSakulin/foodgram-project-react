ba4ce960c7bf5b95332c98f984f942996dd2f085
Александр Сакулин <sakulisakulin@yandex.ru>
2023-07-27 12:15:54 +0300

    fix flake8

--- backend/foodgram/recipes/management/commands/load_data.py
+++ backend/foodgram/recipes/management/commands/load_data.py
@@ -1,7 +1,6 @@
 import json
 
 from django.core.management.base import BaseCommand
-from django.db import connection
 
 from recipes.models import Ingredient
 
@@ -10,24 +9,17 @@ class Command(BaseCommand):
     help = 'Load data from JSON file into database'
 
     def add_arguments(self, parser):
-        parser.add_argument('file', type=str, help='JSON file to load data from')
+        parser.add_argument(
+            'file', type=str, help='JSON file to load data from'
+        )
 
     def handle(self, *args, **options):
 
-        # очищаем данные
-        # Ingredient.objects.all().delete()
-        # with connection.cursor() as cursor:
-        #     cursor.execute('DELETE FROM "recipes_ingredient"')
-        #     cursor.execute(
-        #         "UPDATE sqlite_sequence  SET seq = 0 WHERE name = 'recipes_ingredient'")
-        # Получаем имя файла из аргументов команды
         file_name = options['file']
 
-        # Открываем файл с данными JSON
         with open(file_name, encoding='utf-8') as f:
             data = json.load(f)
 
-        # Создаем экземпляры модели Ingredient на основе данных из файла JSON
         for item in data:
             ingredient = Ingredient(
                 name=item['name'],
@@ -35,5 +27,4 @@ class Command(BaseCommand):
             )
             ingredient.save()
 
-        # Выводим сообщение о завершении процесса загрузки
         self.stdout.write(self.style.SUCCESS('Data loaded successfully.'))
