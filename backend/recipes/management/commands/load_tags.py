00bcc6ae98a0c378609c593b6dced55d8414dcad
Александр Сакулин <sakulisakulin@yandex.ru>
2023-08-07 11:56:21 +0300

    add data tags and fix load_data

--- backend/recipes/management/commands/load_tags.py
+++ backend/recipes/management/commands/load_tags.py
@@ -2,25 +2,25 @@ import json
 
 from django.core.management.base import BaseCommand
 
-from recipes.models import Ingredient
+from recipes.models import Tag
 
 
 class Command(BaseCommand):
-    help = 'Load data from JSON file into database'
+    help = 'Load tags from JSON file into database'
 
     def handle(self, *args, **options):
         with open(
-            'data/ingredients.json',
-            'r',
-            encoding='utf-8'
-        ) as file:
-            data = json.load(file)
+                'data/tags.json',
+                'r',
+                encoding='UTF-8'
+        ) as tags:
+            data = json.load(tags)
         for note in data:
             try:
-                Ingredient.objects.get_or_create(**note)
+                Tag.objects.get_or_create(**note)
                 print(f"{note['name']} в базе")
             except Exception as error:
                 print(f"Ошибка при добавлении {note['name']}.\n"
                       f"Текст - {error}")
 
-        print('Загрузка ингредиентов завершена')
+        print('Загрузка тэгов завершена')
