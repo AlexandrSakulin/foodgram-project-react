d45650a5a35910438bf71255f10dbd171677470c
Александр Сакулин <sakulisakulin@yandex.ru>
2023-08-16 12:03:14 +0300

    ad

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -4,10 +4,10 @@ from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
 from rest_framework.fields import SerializerMethodField
 
-# from foodgram.global_constants import (MAX_AMOUNT_INGRIDIENTS,
-#                                        MAX_TIME_COOKING,
-#                                        MIN_AMOUNT_INGRIDIENTS,
-#                                        MIN_TIME_COOKING)
+from foodgram.global_constants import (MAX_AMOUNT_INGRIDIENTS,
+                                       MAX_TIME_COOKING,
+                                       MIN_AMOUNT_INGRIDIENTS,
+                                       MIN_TIME_COOKING)
 from recipes.models import (Favorite, Ingredient, IngredientInRecipe, Recipe,
                             ShoppingCart, Tag)
 from users.models import Subscribe, User
@@ -116,6 +116,9 @@ class IngredientInRecipeSerializer(serializers.ModelSerializer):
     measurement_unit = serializers.ReadOnlyField(
         source='ingredient.measurement_unit'
     )
+    amount = serializers.ReadOnlyField(
+        source='ingredient.amount'
+    )
 
     class Meta:
         model = IngredientInRecipe
@@ -127,8 +130,7 @@ class RecipeReadSerializer(serializers.ModelSerializer):
 
     tags = TagSerializer(many=True, read_only=True)
     author = UserReadSerializer(read_only=True)
-    # ingredients = IngredientInRecipeSerializer(
-    #     source='recipe_ingredients', many=True)
+    # ingredients = IngredientInRecipeSerializer(source='recipe_ingredients')
     # Если переделываю так, то не подгружает ингридиенты в рецепт
     ingredients = serializers.SerializerMethodField(read_only=True)
     is_favorited = serializers.SerializerMethodField(read_only=True)
@@ -184,7 +186,10 @@ class IngredientInRecipeCreateUpdateSerializer(serializers.ModelSerializer):
     """Ингредиенты в рецепте """
     id = serializers.PrimaryKeyRelatedField(
         queryset=Ingredient.objects.all())
-    amount = serializers.IntegerField()
+    amount = serializers.IntegerField(
+        [MIN_AMOUNT_INGRIDIENTS, 'Минимальное количество ингридиентов 1'],
+        [MAX_AMOUNT_INGRIDIENTS, 'Максимальное количество ингридиентов 100']
+    )
 
     class Meta:
         model = IngredientInRecipe
@@ -199,7 +204,10 @@ class RecipeCreateUpdateSerializer(serializers.ModelSerializer):
     )
     image = Base64ImageField(max_length=None, use_url=True, required=False)
     author = UserReadSerializer(read_only=True, required=False)
-    cooking_time = serializers.IntegerField()
+    cooking_time = serializers.IntegerField(
+        [MIN_TIME_COOKING, 'Минимальное время готовки не менее 1'],
+        [MAX_TIME_COOKING, 'Максимальное время готовки не более 32767']
+    )
 
     def validate(self, data):
 
