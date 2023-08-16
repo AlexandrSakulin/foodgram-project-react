a630f27719bab7aae2a2f31dcdf970ac26795ffe
Александр Сакулин <sakulisakulin@yandex.ru>
2023-08-16 12:36:24 +0300

    push

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,15 +1,15 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db.models import F
 from djoser import serializers as ds
 from drf_extra_fields.fields import Base64ImageField
-from rest_framework import serializers
-from rest_framework.fields import SerializerMethodField
-
 from foodgram.global_constants import (MAX_AMOUNT_INGRIDIENTS,
                                        MAX_TIME_COOKING,
                                        MIN_AMOUNT_INGRIDIENTS,
                                        MIN_TIME_COOKING)
 from recipes.models import (Favorite, Ingredient, IngredientInRecipe, Recipe,
                             ShoppingCart, Tag)
+from rest_framework import serializers
+from rest_framework.fields import SerializerMethodField
 from users.models import Subscribe, User
 
 
@@ -187,8 +187,9 @@ class IngredientInRecipeCreateUpdateSerializer(serializers.ModelSerializer):
     id = serializers.PrimaryKeyRelatedField(
         queryset=Ingredient.objects.all())
     amount = serializers.IntegerField(
-        [MIN_AMOUNT_INGRIDIENTS, 'Минимальное количество ингридиентов 1'],
-        [MAX_AMOUNT_INGRIDIENTS, 'Максимальное количество ингридиентов 100']
+        validators=[
+            MinValueValidator(MIN_AMOUNT_INGRIDIENTS, 'Не менее 1'),
+            MaxValueValidator(MAX_AMOUNT_INGRIDIENTS, 'Не более 32767')]
     )
 
     class Meta:
@@ -205,8 +206,9 @@ class RecipeCreateUpdateSerializer(serializers.ModelSerializer):
     image = Base64ImageField(max_length=None, use_url=True, required=False)
     author = UserReadSerializer(read_only=True, required=False)
     cooking_time = serializers.IntegerField(
-        [MIN_TIME_COOKING, 'Минимальное время готовки не менее 1'],
-        [MAX_TIME_COOKING, 'Максимальное время готовки не более 32767']
+        validators=[
+            MIN_TIME_COOKING, 'Минимальное время готовки не менее 1',
+            MAX_TIME_COOKING, 'Максимальное время готовки не более 32767']
     )
 
     def validate(self, data):
